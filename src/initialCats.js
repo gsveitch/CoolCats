56ad4c862123cff548902d2fa60321f5a4069e4a
Gareth Veitch <garethveitch@gareths-imac.lan>
2021-03-06 10:34:44 -0600

    create initialCats data array

---
+++ src/initialCats.js
@@ -0,0 +1,44 @@
+const initialCats = [
+    {
+        id: 0,
+        thumbnailURL: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/98880a65948559.5b05916bdc9e3.jpg',
+        name: 'John',
+        birthdate: '1940-10-09',
+        ownerName: 'Jane Doe',
+        viewsCount: 0,
+    },
+    {
+        id: 1,
+        thumbnailURL: 'https://images.8tracks.com/cover/i/000/134/856/heather-mattoon-cats-in-clothes-3-600x817-9212.jpg?rect=0,108,600,600&q=98&fm=jpg&fit=max',
+        name: 'Paul',
+        birthdate: '1942-06-18',
+        ownerName: 'Kate Debarros',
+        viewsCount: 0,
+    },
+    {
+        id: 2,
+        thumbnailURL: 'https://tushita.de/images/product_images/popup_images/9783965542716_02.jpg',
+        name: 'George',
+        birthdate: '1943-02-25',
+        ownerName: 'John Doe',
+        viewsCount: 0,
+    },
+    {
+        id: 3,
+        thumbnailURL: 'https://cdn.shopify.com/s/files/1/0421/6969/6424/products/580bannertest_800x.jpg?v=1600570740',
+        name: 'Ringo',
+        birthdate: '1940-07-07',
+        ownerName: 'Jane Doe',
+        viewsCount: 0,
+    },
+    {
+        id: 0,
+        thumbnailURL: 'https://external-preview.redd.it/GhTDElEXwZBZWX-ahknA1U1xBOuGPUi5ihtRukMoKKc.jpg?auto=webp&s=625163dcbbba3fa52bf71c33a591900c2cedee98',
+        name: 'David',
+        birthdate: '1947-01-08',
+        ownerName: 'Kate Debarros',
+        viewsCount: 0,
+    },
+];
+
+export default initialCats;
