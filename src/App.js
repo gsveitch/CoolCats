55a89dabe0b975eef7f4eb47f8bd3b067f533a89
Gareth Veitch <garethveitch@gareths-imac.lan>
2021-03-06 11:33:41 -0600

    slight refactor, sticking with functional components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { useEffect, useState } from 'react';
 import './App.css';
 import initialCats from './initialCats';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      catsCollection: JSON.parse(localStorage.getItem('catsCollection')),
-    };
-  }
+function App() {
+  const [catsCollection, setCatsCollection] = useState(JSON.parse(localStorage.getItem('catsCollection')));
 
-  componentDidMount() {
+  // on initial render, checks if catsCollection already exists in localStorage
+  // if not, then initializes localStorage and state with og cats array from file
+  useEffect(() => {
     if (!localStorage.getItem('catsCollection')) {
       localStorage.setItem('catsCollection', JSON.stringify(initialCats));
-      this.setState({ catsCollection: initialCats });
+      setCatsCollection(initialCats);
     }
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <nav className="navbar navbar-dark bg-dark">
-          <div className="container-fluid">
-            <span className="navbar-text">
-              Cool Cats
-            </span>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+  }, []);
+
+  // listens for changes on the catsCollection state and updates localStorage
+  useEffect(() => {
+    localStorage.setItem('catsCollection', JSON.stringify(catsCollection));
+  }, [catsCollection]);
+
+  return (
+    <div className="App">
+      <nav className="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <span class="navbar-text">
+            Cool Cats
+          </span>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
